cf87550cf9ae30fd1ca6675a347b893f89daacd7
Russell Adams <russell.adams@avast.com>
2021-03-03 15:05:10 -0800

    deploy.sh fixed
    will now deploy cadd with ssl for host "www" as well as add api bits, 
    and should work out of the box.

--- deploys/cloudy_website/deploy_local.sh
+++ deploys/cloudy_website/deploy_local.sh
@@ -1,12 +1,6 @@
   #!/bin/bash
-
-####### NOTE: I am adding this for troubleshotting...
-## sudo has been inserted to allow this to be pasted locally
-## as it runs as root durring instance startup... but later you
-## are "admin"
-
-  #hostnamectl set-hostname www.cloudynet.work
-  #sed '9 a 127.0.0.1   www.cloudynet.work www' /etc/hosts
+  hostnamectl set-hostname www.cloudynet.work
+  sed '9 a 127.0.0.1   www.cloudynet.work www' /etc/hosts
   sudo apt -y update && sudo apt -y upgrade
   sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
   sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
@@ -14,9 +8,13 @@
   sudo apt -y update && sudo apt -y install caddy
   sudo rm /usr/share/caddy/index.html
   sudo aws s3 cp "s3://spotrunner.ig.dev/www/" "/usr/share/caddy/" --recursive
-  #sed -i 's/:80/www.cloudynet.work/g' /etc/caddy/Caddyfile
-  #reboot
+  sudo chown -R admin:admin /usr/share/caddy/api/
+  sed -i 's/:80/www.cloudynet.work/g' /etc/caddy/Caddyfile
+  sudo aws s3 cp s3://spotrunner.ig.dev/api.sh /usr/local/bin/
+  sudo chmod +x  /usr/local/bin/api.sh
+  reboot
 
-
-### Note, removed bits for mangle the hostname and dns, which means
-### Caddy wont do automatic SSL for now.
+  ####### NOTE: I am adding this for troubleshotting...
+  ## sudo has been inserted to allow this to be pasted locally
+  ## as it runs as root durring instance startup... but later you
+  ## are "admin"
