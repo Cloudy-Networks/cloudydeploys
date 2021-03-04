390bea726db164fb1c29f1bd99515915d925c875
Russell Adams <russell.adams@avast.com>
2021-03-03 16:25:21 -0800

    minor

--- deploys/cloudy_website/deploy_local.sh
+++ deploys/cloudy_website/deploy_local.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
-  hostnamectl set-hostname www.cloudynet.work
-  sed '9 a 127.0.0.1   www.cloudynet.work www' /etc/hosts
+  sudo hostnamectl set-hostname www.kinda.cloudynet.work
+  sudo sed '9 a 127.0.0.1   www.kinda.cloudynet.work www' /etc/hosts
   sudo apt -y update && sudo apt -y upgrade
   sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
   sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
@@ -9,10 +9,10 @@
   sudo rm /usr/share/caddy/index.html
   sudo aws s3 cp "s3://spotrunner.ig.dev/www/" "/usr/share/caddy/" --recursive
   sudo chown -R admin:admin /usr/share/caddy/api/
-  sed -i 's/:80/www.cloudynet.work/g' /etc/caddy/Caddyfile
+  sudo sed -i 's/:80/www.kinda.cloudynet.work/g' /etc/caddy/Caddyfile
   sudo aws s3 cp s3://spotrunner.ig.dev/api.sh /usr/local/bin/
   sudo chmod +x  /usr/local/bin/api.sh
-  reboot
+  sudo reboot
 
   ####### NOTE: I am adding this for troubleshotting...
   ## sudo has been inserted to allow this to be pasted locally
